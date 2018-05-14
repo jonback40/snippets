7d36e2a93ce57df88ad90707c9d4f1aa8388ec01
Justin <32679969+justinb40@users.noreply.github.com>
2018-05-14 11:40:35 -0500

    Update dynamic-banner.js

--- dynamic-banner.js
+++ dynamic-banner.js
@@ -15,3 +15,22 @@ if(!$admin.length) {
 
 // Example here 
 suburbancabinets2.javelincms.com
+
+
+// No Page hero example
+(function(){
+	'use strict';
+	// Dynamic hero banner image
+	var $img = $('.page-banner .banner-image .javelin_textItem img'),
+	    $src = $img.attr('src'),
+	    $banner = $('.page-banner .banner-image'),
+	    $admin = $('.is-admin'),
+	    $javelinRegion = $('.page-banner .banner-image .javelin_region'); // gap will appear if whole region isn't removed - using jQuery selector because ie 11 -9 does not support .remove() in vanilla js
+
+	// Hide javelin region when logged out
+	// Add banner image to hero banner
+	if(!$admin.length) {
+	    $banner.css('background-image', 'url('+ $src +')');
+		$javelinRegion.remove();
+	}
+})();
